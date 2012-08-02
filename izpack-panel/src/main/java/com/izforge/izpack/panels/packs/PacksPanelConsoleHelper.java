3b5974110b1c55daa8748dd59492d4e9844c59fd
Damien Richard <damien.richard@arondor.com>
2012-08-02 12:11:47 +0200

    Improve the PackPanelConsoleHelper and change the unsupported console installation test

--- izpack-panel/src/main/java/com/izforge/izpack/panels/packs/PacksPanelConsoleHelper.java
+++ izpack-panel/src/main/java/com/izforge/izpack/panels/packs/PacksPanelConsoleHelper.java
@@ -8,7 +8,12 @@ import com.izforge.izpack.api.data.Pack;
 import com.izforge.izpack.installer.console.PanelConsole;
 import com.izforge.izpack.installer.console.PanelConsoleHelper;
 import com.izforge.izpack.util.Console;
-
+/**
+ * A implementation of PacksPanel for a console installation
+ * TODO use the OSVersion to disable some packs
+ * @author drichard
+ *
+ */
 public class PacksPanelConsoleHelper extends PanelConsoleHelper implements PanelConsole{
 
 	@Override
@@ -21,25 +26,39 @@ public class PacksPanelConsoleHelper extends PanelConsoleHelper implements Panel
 	@Override
 	public boolean runConsole(InstallData installData, Console console) {
 		java.util.List<Pack> selectedPacks = new ArrayList<Pack>();
-		String installationMode = console.prompt("Do you to execute the defaut installation ? Y for yes, N for no", null);
-		if (installationMode != null && installationMode.equals("y") || installationMode.equals("Y")){
+		String installationMode = null;
+
+		do {
+			installationMode = console.prompt("Do you to execute the defaut installation ? Y for yes, N for no", null);
+		}
+
+		while (!isGoodValue(installationMode)); 
+		if (isYes(installationMode)){
 			for (Pack p : installData.getAvailablePacks()){
 				if(p.isRequired() || p.isPreselected()){
 					selectedPacks.add(p);
 				}
 			}
 		}
-		else if(installationMode != null && installationMode.equals("n") || installationMode.equals("N")){
+		else if(isNo(installationMode)){
 			for (Pack p : installData.getAvailablePacks()) {
+
 				if(p.isRequired()){
 					selectedPacks.add(p);
 				}
 				else {
-					String	packToInstall = console.prompt("Do you want to install  [" + p.getName() + "] Y for yes, N for no ", null);
-					if (packToInstall != null && packToInstall.equals("y") || packToInstall.equals("Y")){
+					String packToInstall = null;
+
+					do{
+						packToInstall = console.prompt("Do you want to install  [" + p.getName() + "] Y for yes, N for no ", null);
+					}
+
+					while(!isGoodValue(packToInstall));
+
+					if (isYes(packToInstall)){
 						selectedPacks.add(p);
 					}
-					else if (packToInstall.equals("N") || packToInstall.equals("n")) {
+					else if (isNo(packToInstall)) {
 						console.println("Installation of " + p.getName() + " skipped");
 					}
 
@@ -49,6 +68,25 @@ public class PacksPanelConsoleHelper extends PanelConsoleHelper implements Panel
 		}
 		installData.setSelectedPacks(selectedPacks);
 		return promptEndPanel(installData, console);
+	}
 
+
+	private boolean isNo(String packToInstall) {
+		return packToInstall.equals("N") || packToInstall.equals("n");
+	}
+
+
+	private boolean isYes(String answer) {
+		if (answer != null && answer.equals("y") || answer.equals("Y")){
+			return true;
+		}
+		else return false;
+	}
+
+	private boolean isGoodValue (String answer){
+		if (answer.equals("y") || answer.equals("Y") || answer.equals("n") || answer.equals("N")){
+			return true;
+		}
+		else return false;
 	}
 }
